10082059433ec2c3ccce5d19c5ca0b13ae1b60d8
plummoore <plummoore@gmail.com>
2017-10-10 11:39:36 +0100

    set interval for falling object

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
 let $dog;
+let $ball;
+let $randomObject = [];
 
 $(() => {
 
   const $dog = $('.dog');
   const $object = $('.object');
   const $game = $('.game');
+  //Objects in game
+  const $ballBlue = $('.ball-blue');
+  const $ballGreen = $('.ball-green');
+  const $ballOrange = $('.ball-orange');
+  const $ballPink = $('.ball-pink');
+  const $ballPurple = $('.ball-purple');
+  const $bone = $('.bone');
+  const $gnome = $('.gnome');
+  const $mushroom = $('.mushroom');
+  // const $randomObject = [];
 
+  console.log(typeof $ballBlue);
+  console.log($ballBlue);
 
   $(document).on('keydown', handleKeyCode);
-  fallAnimate();
+  animateFall();
 
 
 
@@ -21,14 +35,21 @@ $(() => {
     if (e.keyCode === 39 && dogLeftValue !== 500) handlePlayerMovement('+');
   }
 
-
   function handlePlayerMovement(operation) {
     $dog.animate({ 'left': `${operation}=20` }, 0);
   }
 
-  function fallAnimate(){
-    const randomWidth = Math.floor(Math.random()*545);
+  function randomWidth() {
+    const randomWidth = Math.floor(Math.random()*500);
     $object.css({'margin-left': randomWidth});
+  }
+
+
+  function animateFall(){
+    // var refreshId = setInterval(animateFall, 2000);
+    fallingTimer();
+    randomWidth();
+    // getRandomObject();
 
     $object.animate({
       top: '+=500'
@@ -41,6 +62,34 @@ $(() => {
   }
 
 
+  function fallingTimer() {
+    setInterval(animateFall, 3000);
+  }
 
+
+
+  function getRandomObject(){
+    const items = ['ball-blue', 'ball-green', 'ball-orange', 'ball-pink', 'ball-purple', 'bone', 'gnome', 'mushroom'];
+    // const items = [$ballBlue, $ballGreen, $ballOrange, $ballPink, $ballPurple, $bone, $gnome, $mushroom];
+    const $randomItem = items[Math.floor(Math.random() * items.length)];
+    console.log($randomItem);
+    // $randomObject.push(randomItem);
+    // console.log($randomObject);
+    // $object.css({'width', 'height', 'background-image' : randomItem});
+    // $object.css = (['width', 'height', 'background-image' : $randomItem]);
+
+    $object.css({'class': $randomItem});
+    console.log($object);
+
+  }
 
 });
+
+
+// function chooseBall() {
+//   const $ball = ['images/ball-blue.png', 'images/ball-green.png', 'images/ball-orange.png', 'images/ball-pink.png', 'images/ball-purple.png'];
+// }
+
+//set interval 2secs
+//append a new div to the parent (game div)
+//as you append - that div needs to have certain classes
